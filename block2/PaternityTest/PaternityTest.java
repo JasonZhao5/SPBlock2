418e417533b734e21b7fbdbd69a59222e8a69b33
JasonZ <jasonjzjc@gmail.com>
2024-03-25 17:06:03 +1030

    block2

---
+++ block2/PaternityTest/PaternityTest.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PaternityTest {
+    ArrayList<String> menArray;
+    HashSet<Integer> resultValues = new HashSet<>();
+    String childString;
+
+    public int[] possibleFathers(String child, String mother, String[] men) {
+        int m = child.length() / 2;
+        ArrayList<Integer> sorts = new ArrayList<>();
+        menArray = new ArrayList<>(Arrays.asList(men));
+        childString = child;
+        generateCombinations(0, m, sorts);
+        return resultValues.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    void generateCombinations(int start, int m, ArrayList<Integer> sorts) {
+        if (sorts.size() == m) {
+            doTest(sorts);
+        }
+        for (int i = start; i < childString.length(); i++) {
+            sorts.add(i);
+            generateCombinations(i + 1, m, sorts);
+            sorts.remove(sorts.size() - 1);
+        }
+    }
+
+    void doTest(List<Integer> sorts) {
+        sorts = sorts.stream().sorted(Comparator.comparingInt(Integer::intValue).reversed()).collect(Collectors.toList());
+        for (int i = 0; i < menArray.size(); i++) {
+            StringBuilder childBuilder = new StringBuilder(childString);
+            StringBuilder manBuilder = new StringBuilder(menArray.get(i));
+            for (Integer sort : sorts) {
+                childBuilder.deleteCharAt(sort);
+                manBuilder.deleteCharAt(sort);
+            }
+            if (manBuilder.compareTo(childBuilder) == 0) {
+                resultValues.add(i);
+            }
+        }
+    }
+
+//    public int[] possibleFathers(String child, String mother, String[] men)
+//    {
+//        int[] poss = new int[men.length];
+//
+//        int nf = 0;
+//        for (int i = 0; i < men.length; i++)
+//        {
+//            int fc = 0, mc = 0;
+//            for (int j = 0; j < men[i].length(); j++)
+//            {
+//
+//            }
+//            if (fc == mc) poss[nf++] = i;
+//        }
+//        int[] res = new int[nf];
+//        for (int i = 0; i < nf; i++) res[i] = poss[i];
+//        return res;
+//    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new PaternityTest().possibleFathers("WXETPYCHUWSQEMKKYNVP",
+                "AXQTUQVAUOSQEEKCYNVP",
+        new String[] { "WNELPYCHXWXPCMNKDDXD",
+                "WFEEPYCHFWDNPMKKALIW",
+                "WSEFPYCHEWEFGMPKIQCK",
+                "WAEXPYCHAWEQXMSKYARN",
+                "WKEXPYCHYWLLFMGKKFBB" }
+
+        )));
+    }
+
+}
+
---
+++ block2/QuickSums/QuickSums.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+
+public class QuickSums {
+
+
+    public int minSums(String numbers, int sum) {
+        int min = -1;
+        return search(numbers, sum, min, 0, 0, 0);
+    }
+
+    int search(String numbers, int sum, int min, int index, int count, long temp) {
+        if (index == numbers.length()) {
+            System.out.println("leaf = " + temp);
+            if (temp == sum && (min == -1 || count - 1 < min)) {
+                min = count - 1;
+            }
+            System.out.println("min = " + min);
+            return min;
+        }
+
+        for (int i = index; i < numbers.length(); ++i) {
+            min = search(numbers, sum, min, i + 1, count + 1, temp + Long.parseLong(numbers.substring(index, i + 1)));
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        QuickSums quickSums = new QuickSums();
+        System.out.println(quickSums.minSums("303",
+                6));
+    }
+}
